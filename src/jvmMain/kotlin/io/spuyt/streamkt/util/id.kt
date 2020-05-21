9e9fe770a7fb4c17af9cbf66aa59686b1d7e1cb3
Spuyt <63584831+spuyt@users.noreply.github.com>
2020-05-21 10:51:52 +0200

    Revert "Basics (#1)" (#2)
    This reverts commit 2666e8d93d7fcbfbba310cd876e99024ef1f3bf9.

--- src/jvmMain/kotlin/io/spuyt/streamkt/util/id.kt
+++
@@ -1,6 +0,0 @@
-package io.spuyt.streamkt.util
-
-// get the unix timestamp in milliseconds, need to override in platform specific libraries
-actual fun unix():Long{
-    return System.currentTimeMillis()
-}
